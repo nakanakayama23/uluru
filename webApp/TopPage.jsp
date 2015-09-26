d82e182d23d6612e6a456623c947d88bf5643d4c
Z <di0rdna18@gmail.com>
2015-09-27 03:51:45 +0900

    簡単なやり取りを行うServletとjspの実装

--- webApp/TopPage.jsp
+++ webApp/TopPage.jsp
@@ -1,22 +1,39 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>トップページ</title>
+<title>Uluru</title>
 </head>
 <body>
-<h2>トップページ</h2>
+	<h2>Uluru</h2>
 
-<form method="post" action="topPage">
-好きな食べ物を選んでください。
-<select name="food">
-	<option value="3">ラーメン</option>
-	<option value="2">カレーライス</option>
-	<option value="1">納豆ごはん</option>
-</select>
-</form>
+	<%
+		String answer = "";
+		if (request.getAttribute("answer") != null) {
+			answer = (String) request.getAttribute("answer");
+		}
+	%>
 
+	<form method="post" action="TopPage">
+		好きな食べ物を選んでください。 <select name="food" onchange="submit()">
+			<option value="" <%if (answer.equals("")) {%> selected <%}%>>選択してください</option>
+			<option value="ラーメン" <%if (answer.equals("ラーメン")) {%> selected <%}%>>ラーメン</option>
+			<option value="カレーライス" <%if (answer.equals("カレーライス")) {%> selected
+				<%}%>>カレーライス</option>
+			<option value="納豆ごはん" <%if (answer.equals("納豆ごはん")) {%> selected
+				<%}%>>納豆ごはん</option>
+		</select>
+	</form>
+
+	<br>
+	<%
+		if (!answer.equals("")) {
+	%>
+	あなたが選択した食べ物は<%=answer%>です。
+	<%
+		}
+	%>
 </body>
 </html>
--- webApp/WEB-INF/src/main/servlet/MainServlet.java
+++ webApp/WEB-INF/src/main/servlet/MainServlet.java
@@ -3,6 +3,7 @@ package main.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,38 +17,39 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/MainServlet")
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainServlet() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MainServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(true);
-		session.setAttribute("testAttribute", 3 + 5);
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Hello World!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1>Hello World!</h1>");
-		out.println("</body>");
-		out.println("</html>");	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+
+		String answer = request.getParameter("food");
+		if (!answer.equals("")) {
+			request.setAttribute("answer", answer);
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("TopPage.jsp");
+		dispatcher.forward(request, response);
 	}
 }
