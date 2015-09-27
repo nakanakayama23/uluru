d5e33a23721e4dbe8d37d588204c52be4897ca7b
Z <di0rdna18@gmail.com>
2015-09-27 11:13:53 +0900

    簡単なやり取りを行うServlet、jspを実装（+修正）

--- webApp/WEB-INF/src/main/servlet/MainServlet.java
+++ webApp/WEB-INF/src/main/servlet/MainServlet.java
@@ -1,7 +1,6 @@
 package main.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MainServlet
@@ -44,10 +42,13 @@ public class MainServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 
-		String answer = request.getParameter("food");
-		if (!answer.equals("")) {
-			request.setAttribute("answer", answer);
+		String answer = "";
+		if(request.getParameter("food")!=null)
+		{
+		answer = request.getParameter("food");
 		}
+		
+			request.setAttribute("answer", answer);
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("TopPage.jsp");
 		dispatcher.forward(request, response);
